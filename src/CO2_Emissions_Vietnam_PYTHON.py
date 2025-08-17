3ee476f5c1e3e3c8635186f291f1b464df0439f9
b1ngusbangus <156984703+b1ngusbangus@users.noreply.github.com>
2025-08-17 00:25:10 -0700

    Update CO2_Emissions_Vietnam_PYTHON.py

--- src/CO2_Emissions_Vietnam_PYTHON.py
+++ src/CO2_Emissions_Vietnam_PYTHON.py
@@ -15,25 +15,25 @@ if selected == "Line Plots":
         lp1.write("Global CO2 Emissions Per Person (1800-2019)")
         lp2.write("Top 10 Emissions-producing Countries in 2010 (1900–2014)")
     with lp1:
-        image_url1 = "https://raw.githubusercontent.com/b1ngusbangus/group_project_eep105_su2025/blob/main/png/World_CO2.png"
+        image_url1 = "https://raw.githubusercontent.com/b1ngusbangus/group_project_eep105_su2025/main/png/World_CO2.png"
         st.image(image_url1, caption="Global CO2 Emissions Per Person (1800-2019)")
     with lp2:
-        image_url2 = "https://raw.githubusercontent.com/b1ngusbangus/group_project_eep105_su2025/blob/main/png/Top10_CO2_lineplot.png"
+        image_url2 = "https://raw.githubusercontent.com/b1ngusbangus/group_project_eep105_su2025/main/png/Top10_CO2_lineplot.png"
         st.image(image_url2, caption="Top 10 Emissions-producing Countries in 2010 (1900–2014)")
 if selected == "Tile Plots":
     tp = st.columns(1)
     with st.container():
         tp.write("Top 10 CO₂ Emission-producing Countries\nOrdered by Emissions Produced in 2014")
     with tp:
-        image_url3 = "https://raw.githubusercontent.com/b1ngusbangus/group_project_eep105_su2025/blob/main/png/Top10_CO2_tile.png"
+        image_url3 = "https://raw.githubusercontent.com/b1ngusbangus/group_project_eep105_su2025/main/png/Top10_CO2_tile.png"
         st.image(image_url3, caption="Top 10 CO₂ Emission-producing Countries\nOrdered by Emissions Produced in 2014")
         
 if selected == "Facet Plots":
     fp = st.columns(1)
     with st.container():
-        tp.write("Distribution of Indications by Year and Value")
+        fp.write("Distribution of Indications by Year and Value")
     with fp:
-        image_url4 = "https://raw.githubusercontent.com/b1ngusbangus/group_project_eep105_su2025/blob/main/png/Agg_facet_plot.png"
+        image_url4 = "https://raw.githubusercontent.com/b1ngusbangus/group_project_eep105_su2025/main/png/Agg_facet_plot.png"
         st.image(image_url4, caption="Distribution of Indications by Year and Value")
         
 if selected == "Scatterplots":
@@ -44,12 +44,12 @@ if selected == "Scatterplots":
         sc2.write("Vietnam CO2 Emissions and Temperature, Unscaled (1980-2014)")
         sc3.write("Vietnam CO2 Emissions and Temperature, Scaled (1980–2014)")
     with sc1:
-        image_url = "https://raw.githubusercontent.com/b1ngusbangus/group_project_eep105_su2025/blob/main/png/CO2_temp_Vietnam_facet.png"
+        image_url = "https://raw.githubusercontent.com/b1ngusbangus/group_project_eep105_su2025/main/png/CO2_temp_Vietnam_facet.png"
         st.image(image_url, caption="Vietnam CO2 Emissions and Temperature, separated")
     with sc2:
-        image_url = "https://raw.githubusercontent.com/b1ngusbangus/group_project_eep105_su2025/blob/main/png/lin_reg_unscaled.png"
+        image_url = "https://raw.githubusercontent.com/b1ngusbangus/group_project_eep105_su2025/main/png/lin_reg_unscaled.png"
         st.image(image_url, caption="Vietnam CO2 Emissions and Temperature, Unscaled (1980-2014)")
     with sc3:
-        image_url = "https://raw.githubusercontent.com/b1ngusbangus/group_project_eep105_su2025/blob/main/png/Vietnam_emissions_temp_scaled.png"
+        image_url = "https://raw.githubusercontent.com/b1ngusbangus/group_project_eep105_su2025/main/png/Vietnam_emissions_temp_scaled.png"
         st.image(image_url, caption="Vietnam CO2 Emissions and Temperature, Scaled (1980–2014)")
 
