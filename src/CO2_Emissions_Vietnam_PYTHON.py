7feec00ce3ae1d3faf933dfa4bea11e40e040873
b1ngusbangus <156984703+b1ngusbangus@users.noreply.github.com>
2025-08-16 23:05:49 -0700

    Update CO2_Emissions_Vietnam_PYTHON.py

--- src/CO2_Emissions_Vietnam_PYTHON.py
+++ src/CO2_Emissions_Vietnam_PYTHON.py
@@ -2,6 +2,7 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import streamlit as st
 import seaborn as sb
 import seaborn as sns
 import pickle
@@ -14,6 +15,15 @@ vietnam_disasters = pd.read_csv("disasters_vietnam.csv")
 temp = pd.read_csv("temperature_vietnam.csv")
 gdp = pd.read_csv("gdp_pcap.csv")
 energy = pd.read_csv('energy_per_person.csv')
+
+# STREAMLIT
+with st.sidebar:
+    selected = option_menu(
+      menu_title = "Main Menu",
+      options = ["Global Emissions","Top 10 Countries","Tile Plot","Facet Plot","Scatterplots"],
+      menu_icon = "cast",
+      default_index = 0
+    )
 
 # DATA WRANGLING
 
