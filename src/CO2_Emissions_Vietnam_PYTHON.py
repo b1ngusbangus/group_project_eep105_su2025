7aaa509330d91e51c3cc1204ab4d6c16d0462a97
katiec77 <160828618+katiec77@users.noreply.github.com>
2025-08-16 22:55:04 -0700

    Update CO2_Emissions_Vietnam_PYTHON.py

--- src/CO2_Emissions_Vietnam_PYTHON.py
+++ src/CO2_Emissions_Vietnam_PYTHON.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import streamlit as st
 import pandas as pd
 import numpy as np
 import seaborn as sb
@@ -13,4 +14,12 @@ vietnam_disasters = pd.read_csv("disasters_vietnam.csv")
 temp = pd.read_csv("temperature_vietnam.csv")
 gdp = pd.read_csv("gdp_pcap.csv")
 energy = pd.read_csv('energy_per_person.csv')
+
+with st.sidebar:
+    selected = option_menu(
+      menu_title = "Main Menu",
+      options = ["Global Emissions","Top 10 Countries","Tile Plot","Facet Plot","Scatterplots"],
+      menu_icon = "cast",
+      default_index = 0
+    )
 
