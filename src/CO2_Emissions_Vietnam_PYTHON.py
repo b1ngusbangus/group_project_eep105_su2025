535638bea0f6711ad004d12c2d5a09813c70f3a4
b1ngusbangus <156984703+b1ngusbangus@users.noreply.github.com>
2025-08-16 22:37:19 -0700

    Update CO2_Emissions_Vietnam_PYTHON.py

--- src/CO2_Emissions_Vietnam_PYTHON.py
+++ src/CO2_Emissions_Vietnam_PYTHON.py
@@ -1,1 +1,16 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sb
+import seaborn as sns
+import pickle
+from pathlib import Path
+from PIL import Image
+
+#getting data through reading their csvs
+co2pp = pd.read_csv("yearly_co2_emissions_1000_tonnes.csv")
+vietnam_disasters = pd.read_csv("disasters_vietnam.csv")
+temp = pd.read_csv("temperature_vietnam.csv")
+gdp = pd.read_csv("gdp_pcap.csv")
+energy = pd.read_csv('energy_per_person.csv')
 
